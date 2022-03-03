3dd53683465c05c0054e5e529c7cb5bafec9fd2a
Denny Chandra <denny.c.chandra@gmail.com>
2022-03-03 18:38:27 +1100

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,42 @@
-function computerPlay() {
+// function to randomise the computer's move
+const computerPlay = () => {
     const result = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random() * 3);
     return result[index];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    switch (true) {
+        case (playerSelection === computerSelection) :
+            return "It's a draw!";
+            break;
+       case (playerSelection === "Rock" && computerSelection === "Scissors") :
+            return "You Win! Rock beats Scissors";
+            break;
+        case (playerSelection === "Rock" && computerSelection === "Paper") :
+            return "You Lose! Paper beats Rock";
+            break;
+        case (playerSelection === "Paper" && computerSelection === "Scissors") :
+            return "You Lose! Scissors beats Paper";
+            break;
+        case (playerSelection === "Paper" && computerSelection === "Rock") :
+            return "You Win! Paper beats Rock";
+            break;
+        case (playerSelection === "Scissors" && computerSelection === "Paper") :
+            return "You Win! Scissors beats Paper";
+            break;
+        case (playerSelection === "Scissors" && computerSelection === "Rock") :
+            return "You Lose! Rock beats Scissors";
+            break;
+     
+
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
